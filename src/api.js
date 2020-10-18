57ddfc5d7de69a9a76346c5ce8e87277ee414915
sunnylqm <sunnylqm@gmail.com>
2020-10-18 12:16:15 +0800

    Add token when upload

--- src/api.js
+++ src/api.js
@@ -147,6 +147,10 @@ async function uploadFile(fn, key) {
       realUrl,
       {
         formData,
+        headers: {
+          'User-Agent': userAgent,
+          'X-AccessToken': session ? session.token : '',
+        },
       },
       (err, resp, body) => {
         if (err) {
